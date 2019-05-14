b6e1caedee686b60bee7912a919d5e1cf5d2da96
or <alpaca623@gmail.com>
2019-05-14 22:50:52 +0900

    add basic login view

---
+++ src/views/login/LoginPresenter.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+import PropTypes from "prop-types";
+
+const Container = styled.View``;
+
+const Sample = styled.Text``;
+
+function LoginPresenter() {
+  return (
+    <Container>
+      <Sample>login view</Sample>
+    </Container>
+  );
+}
+
+export default LoginPresenter;
