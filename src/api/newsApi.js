139ed404c63bd4a2baf5b8a1f54a79a79012a0e3
or <alpaca623@gmail.com>
2019-05-19 16:11:02 +0900

    fixed wrong request parameter setting

--- src/api/newsApi.js
+++ src/api/newsApi.js
@@ -1,24 +1,46 @@
 import axios from "axios";
 
-const axios_instance = type =>
-  axios.create({
-    baseURL: `https://newsapi.org/v2/${type}`,
-    params: {
-      apikey: "a8684d4696a04a038c1caeea926eb4f2",
-      country: "kr"
-    }
-  });
+const axios_instance = axios.create({
+  baseURL: `https://newsapi.org/v2/top-headlines`,
+  params: {
+    apikey: "a8684d4696a04a038c1caeea926eb4f2",
+    country: "kr"
+  }
+});
+/***************************
+ * interceptors
+ * 요청, 응답 관련 파라미터 체크(5.19)
+ ***************************/
+// axios_instance.interceptors.request.use(
+//   config => {
+//     console.log(config);
+//     return config;
+//   },
+//   error => console.log(error)
+// );
+
+// axios_instance.interceptors.response.use(
+//   config => {
+//     console.log(config);
+//     return config;
+//   },
+//   error => console.log(error)
+// );
 
 export const requestData = {
   headline: (page, pageSize, query, category) => {
-    const params = {
+    const data = {
       page: page,
-      q: query,
       pageSize: pageSize
     };
-    if (params.category !== "") {
-      params["category"] = category;
+    console.log(category);
+    if (category !== "All") {
+      data["category"] = category;
     }
-    return axios_instance("top-headlines").get("", params);
+    if (query !== null) {
+      data["q"] = query;
+    }
+
+    return axios_instance.get("", { params: data });
   }
 };
